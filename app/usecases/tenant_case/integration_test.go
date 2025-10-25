838a22a4d13d13a22b0cd4f2163a726b0bb1a44a
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2025-10-25 02:27:36 -0300

    refactor: rename test function for tenant creation to improve clarity

--- app/usecases/tenant_case/integration_test.go
+++ app/usecases/tenant_case/integration_test.go
@@ -32,30 +32,33 @@ func TestMySuite(t *testing.T) {
 	suite.Run(t, new(TestSuite))
 }
 
-func (t *TestSuite) Test_Integration_Create__should_create() {
-	Body := tenant_case.CreateInput{
-		Name:     "Name",
-		UserName: "UserName",
-		Phone:    "Phone",
-		Email:    "email@email.com",
-	}
+func (t *TestSuite) Test_TenantIntegration_Create() {
+	t.Run("should be able to create", func() {
+		Body := tenant_case.CreateInput{
+			Name:     "Name",
+			UserName: "UserName",
+			Phone:    "Phone",
+			Email:    "email@email.com",
+		}
 
-	var response tenant_case.CreateOutput
-	httpexpect.Default(t.T(), fixtures.AppURL).
-		Request(http.MethodPost, fixtures.Tenant.URI).
-		WithJSON(Body).
-		Expect().Status(http.StatusCreated).
-		JSON().Decode(&response)
+		var response tenant_case.CreateOutput
+		httpexpect.Default(t.T(), fixtures.AppURL).
+			Request(http.MethodPost, fixtures.Tenant.URI).
+			WithJSON(Body).
+			Expect().Status(http.StatusCreated).
+			JSON().Decode(&response)
 
-	token := fixtures.Auth.UserToken(t.T(), response.UserID)
+		token := fixtures.Auth.UserToken(t.T(), response.UserID)
 
-	httpexpect.Default(t.T(), fixtures.AppURL).
-		Request(http.MethodGet, fixtures.Tenant.URI+response.ID).
-		WithHeader("Authorization", "Bearer "+token).
-		Expect().Status(http.StatusOK).
-		JSON().Object().
-		ContainsSubset(map[string]any{
-			"id":   response.ID,
-			"name": Body.Name,
-		})
+		httpexpect.Default(t.T(), fixtures.AppURL).
+			Request(http.MethodGet, fixtures.Tenant.URI+response.ID).
+			WithHeader("Authorization", "Bearer "+token).
+			Expect().Status(http.StatusOK).
+			JSON().Object().
+			ContainsSubset(map[string]any{
+				"id":   response.ID,
+				"name": Body.Name,
+			})
+	})
+
 }
