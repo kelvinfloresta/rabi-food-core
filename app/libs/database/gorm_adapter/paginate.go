91e3e2a848e978e75df20f40fafec30b3cfd35c3
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2025-10-26 16:29:55 -0300

    fix: streamline goroutine management in Paginate function

--- app/libs/database/gorm_adapter/paginate.go
+++ app/libs/database/gorm_adapter/paginate.go
@@ -7,25 +7,22 @@ import (
 	"gorm.io/gorm"
 )
 
+// Paginate performs pagination on the given GORM query.
 func Paginate(query *gorm.DB, count *int64, data any, paginate database.PaginateInput) error {
 	var wg sync.WaitGroup
 	countErr := error(nil)
 	countSession := query.Session(&gorm.Session{})
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
+	wg.Go(func() {
 		result := countSession.Count(count)
 		countErr = result.Error
-	}()
+	})
 
 	paginateErr := error(nil)
 	paginateSession := query.Session(&gorm.Session{})
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
+	wg.Go(func() {
 		result := paginateSession.Limit(paginate.PageSize).Offset(paginate.Offset()).Find(data)
 		paginateErr = result.Error
-	}()
+	})
 
 	wg.Wait()
 	if countErr != nil {
