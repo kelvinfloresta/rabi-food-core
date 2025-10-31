00fc0a53418bb28c3f943a13fd2c734b00c7ca1b
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2025-10-31 20:16:52 -0300

    fix: remove unnecessary blank lines in Delete method for cleaner code

--- app/libs/database/gateways/category_gateway/gorm_delete.go
+++ app/libs/database/gateways/category_gateway/gorm_delete.go
@@ -6,9 +6,11 @@ import (
 
 func (g *GormCategoryGatewayAdapter) Delete(filter DeleteFilter) (bool, error) {
 	query := g.DB.Conn.Where("id = ?", filter.ID)
+
 	if filter.TenantID != "" {
 		query = query.Where("tenant_id = ?", filter.TenantID)
 	}
+
 	result := query.Delete(&models.Category{})
 
 	return result.RowsAffected > 0, result.Error
