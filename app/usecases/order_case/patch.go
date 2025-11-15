4183e5d435539e6af7de5d3fecee82f38d99c374
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2025-11-15 01:46:27 -0300

    refactor: enhance Patch method to include status validation for delivery, fulfillment, and payment

--- app/usecases/order_case/patch.go
+++ app/usecases/order_case/patch.go
@@ -3,8 +3,15 @@ package order_case
 import (
 	"context"
 	"rabi-food-core/app_context"
+	"rabi-food-core/domain/order"
 	g "rabi-food-core/libs/database/gateways/order_gateway"
+	"rabi-food-core/libs/errs"
 )
+
+type PatchFilter struct {
+	ID       string `json:"id"`
+	TenantID string `json:"tenantId"`
+}
 
 func (c *OrderCase) Patch(
 	ctx context.Context,
@@ -16,5 +23,35 @@ func (c *OrderCase) Patch(
 		filter.TenantID = session.TenantID
 	}
 
-	return c.gateway.Patch(filter, values)
+	deliveryStatusIn := []order.DeliveryStatus(nil)
+	if values.DeliveryStatus != "" {
+		deliveryStatusIn = values.DeliveryStatus.GetPrerequisites()
+		if len(deliveryStatusIn) == 0 {
+			return false, errs.StatusNotModifiable(string(values.DeliveryStatus))
+		}
+	}
+
+	fulfillmentStatusIn := []order.FulfillmentStatus(nil)
+	if values.FulfillmentStatus != "" {
+		fulfillmentStatusIn = values.FulfillmentStatus.GetPrerequisites()
+		if len(fulfillmentStatusIn) == 0 {
+			return false, errs.StatusNotModifiable(string(values.FulfillmentStatus))
+		}
+	}
+
+	paymentStatusIn := []order.PaymentStatus(nil)
+	if values.PaymentStatus != "" {
+		paymentStatusIn = values.PaymentStatus.GetPrerequisites()
+		if len(paymentStatusIn) == 0 {
+			return false, errs.StatusNotModifiable(string(values.PaymentStatus))
+		}
+	}
+
+	return c.gateway.Patch(g.PatchFilter{
+		ID:                  filter.ID,
+		TenantID:            filter.TenantID,
+		DeliveryStatusIn:    deliveryStatusIn,
+		FulfillmentStatusIn: fulfillmentStatusIn,
+		PaymentStatusIn:     paymentStatusIn,
+	}, values)
 }
